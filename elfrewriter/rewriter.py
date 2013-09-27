562f9dd4b1d1919b04de606637a7bc0195b91b6d
David Cournapeau <cournape@gmail.com>
2013-09-27 14:40:36 +0100

    FEAT: add support for rpath rewriting.
    We also handle DT_RPATH and DT_RUNPATH correctly now, following the same
    precedence as the linux dynamic loader.

--- elfrewriter/rewriter.py
+++ elfrewriter/rewriter.py
@@ -1,3 +1,5 @@
+import subprocess
+
 from elftools.common.py3compat import bytes2str
 from elftools.elf.elffile import ELFFile
 from elftools.elf.dynamic import DynamicSection
@@ -8,6 +10,10 @@ from .utils import is_elf
 PATHSEP = ":"
 
 def _get_rpath_entry(elf_handle):
+    """
+    Returns the value of the DT_RPATH entry in the ELF header, or None if not
+    set.
+    """
     for section in elf_handle.iter_sections():
         if not isinstance(section, DynamicSection):
             continue
@@ -16,6 +22,20 @@ def _get_rpath_entry(elf_handle):
             # XXX: is there only one RD_RPATH entry ?
             if tag.entry.d_tag == 'DT_RPATH':
                 return bytes2str(tag.rpath)
+
+def _get_runpath_entry(elf_handle):
+    """
+    Returns the value of the DT_RUNPATH entry in the ELF header, or None if not
+    set.
+    """
+    for section in elf_handle.iter_sections():
+        if not isinstance(section, DynamicSection):
+            continue
+
+        for tag in section.iter_tags():
+            # XXX: is there only one RD_RUNPATH entry ?
+            if tag.entry.d_tag == 'DT_RUNPATH':
+                return bytes2str(tag.runpath)
 
 def _get_dependencies(elf_handle):
     shared_libraries = []
@@ -33,13 +53,31 @@ class ELFRewriter(object):
     ELFRewriter can be used to query and change elf properties relevant for
     relocatability.
 
-    Concretely, you can query/modify the following:
+    Parameters
+    ----------
+    filename: str
+        The path to the ELF file
+    force_dt_rpath: bool
+        When modifying the elf file, the new rpath entry is written to
+        DT_RUNPATH. If force_dt_rpath is set to True, both DT_RUNPATH and
+        DT_RPATH are written (to the same value).
 
-        - rpaths sections
-        - dependencies
+    Note
+    ----
+    ELF has two ways of defining rpath: DT_RPATH or DT_RUNPATH. DT_RUNPATH is
+    strictly better than DT_RPATH, but newer.
+
+    If *both* DT_RPATH and DT_RUNPATH as set, only DT_RUNPATH is considered
+    (same rule as the ELF-loader). IOW:
+
+        - if only DT_RPATH is set, ELFRewriter.rpaths corresponds to DT_RPATH
+        - if only DT_RUNPATH is set, ELFRewriter.rpaths corresponds to DT_RUNPATH
+        - if both are defined, DT_RPATH is ignored, and ELFRewriter.rpaths
+          corresponds to DT_RUNPATH.
     """
-    def __init__(self, filename):
+    def __init__(self, filename, force_dt_rpath=False):
         self.filename = filename
+        self._force_dt_rpath = force_dt_rpath
 
         if not is_elf(filename):
             raise ELFRewriterError("File %s is not an ELF file." % (filename,))
@@ -47,15 +85,34 @@ class ELFRewriter(object):
         with open(filename, "rb") as fp:
             self._elf_handle = ELFFile(fp)
             rpath = _get_rpath_entry(self._elf_handle)
-            self._rpaths = rpath.split(PATHSEP)
+            runpath = _get_runpath_entry(self._elf_handle)
+            if runpath is None:
+                if rpath is None or len(rpath) == 0:
+                    self.rpaths = []
+                else:
+                    self.rpaths = rpath.split(PATHSEP)
+            else:
+                if runpath is None or len(runpath) == 0:
+                    self.rpaths = []
+                else:
+                    self.rpaths = runpath.split(PATHSEP)
             self._dependencies = _get_dependencies(self._elf_handle)
 
+    def commit(self):
+        try:
+            if self._force_dt_rpath:
+                cmd = ["patchelf", "--force-rpath", "--set-rpath",
+                       self.rpaths_string, self.filename]
+            else:
+                cmd = ["patchelf", "--set-rpath", self.rpaths_string,
+                       self.filename]
+            subprocess.check_call(cmd)
+        except OSError:
+            raise ELFRewriterError("Could not write changes back to {0}".format(self.filename))
+
     @property
-    def rpaths(self):
-        """
-        The list of rpaths.
-        """
-        return self._rpaths
+    def rpaths_string(self):
+        return PATHSEP.join(self.rpaths)
 
     @property
     def dependencies(self):
--- elfrewriter/tests/test_rewriter.py
+++ elfrewriter/tests/test_rewriter.py
@@ -1,9 +1,15 @@
+import contextlib
+import shutil
+import subprocess
+import tempfile
 import unittest
 
 import os.path as op
 
+from elftools.elf.elffile import ELFFile
+
 from ..errors import ELFRewriterError
-from ..rewriter import ELFRewriter
+from ..rewriter import ELFRewriter, _get_rpath_entry, _get_runpath_entry
 
 DATA_DIR = op.join(op.dirname(__file__), "data")
 
@@ -13,10 +19,30 @@ FILES_TO_RPATHS = {
     MAIN_ABS_RPATH: ["/yomama"],
 }
 
+FILES_TO_RPATHS_STRING = {
+    MAIN_ABS_RPATH: "/yomama",
+}
+
 FILES_TO_DEPENDENCIES = {
     MAIN_ABS_RPATH: ["libc.so.6"],
 
 }
+
+def _ensure_has_patchelf():
+    try:
+        res = subprocess.call(["patchelf", "--version"], stdout=subprocess.PIPE)
+        return res == 0
+    except OSError:
+        return False
+
+HAS_PATCHELF = _ensure_has_patchelf()
+
+@contextlib.contextmanager
+def mkdtemp():
+    d = tempfile.mkdtemp()
+    yield d
+    shutil.rmtree(d)
+
 
 class TestELFRewriter(unittest.TestCase):
     def test_invalid_file(self):
@@ -31,7 +57,63 @@ class TestELFRewriter(unittest.TestCase):
             rewriter = ELFRewriter(f)
             self.assertEqual(rewriter.rpaths, r_rpaths)
 
+    def test_rpaths_string(self):
+        for f, r_rpath_string in FILES_TO_RPATHS_STRING.items():
+            rewriter = ELFRewriter(f)
+            self.assertEqual(rewriter.rpaths_string, r_rpath_string)
+
     def test_dependencies(self):
         for f, r_dependencies in FILES_TO_DEPENDENCIES.items():
             rewriter = ELFRewriter(f)
             self.assertEqual(rewriter.dependencies, r_dependencies)
+
+    @unittest.skipIf(not HAS_PATCHELF, "Cannot run rpath writing tests without patchelf")
+    def test_strip_rpath(self):
+        with mkdtemp() as d:
+            copy = op.join(d, op.basename(MAIN_ABS_RPATH))
+            shutil.copy(MAIN_ABS_RPATH, copy)
+
+            rewriter = ELFRewriter(copy)
+            rewriter.rpaths = []
+            rewriter.commit()
+
+            rewriter = ELFRewriter(copy)
+            self.assertEqual(rewriter.rpaths, [])
+            self.assertEqual(rewriter.rpaths_string, "")
+
+    @unittest.skipIf(not HAS_PATCHELF, "Cannot run rpath writing tests without patchelf")
+    def test_change_rpath(self):
+        with mkdtemp() as d:
+            copy = op.join(d, op.basename(MAIN_ABS_RPATH))
+            shutil.copy(MAIN_ABS_RPATH, copy)
+
+            rewriter = ELFRewriter(copy)
+            rewriter.rpaths.insert(0, "floupiga")
+            rewriter.commit()
+
+            rewriter = ELFRewriter(copy)
+            self.assertEqual(rewriter.rpaths, ["floupiga"] + FILES_TO_RPATHS[MAIN_ABS_RPATH])
+            self.assertEqual(rewriter.rpaths_string, "floupiga:/yomama")
+
+            with open(copy, "rb") as fp:
+                elf_handle = ELFFile(fp)
+                self.assertEqual(_get_rpath_entry(elf_handle), None)
+                self.assertEqual(_get_runpath_entry(elf_handle), "floupiga:/yomama")
+
+    @unittest.skipIf(not HAS_PATCHELF, "Cannot run rpath writing tests without patchelf")
+    def test_change_rpath_force_dt_rpath(self):
+        with mkdtemp() as d:
+            copy = op.join(d, op.basename(MAIN_ABS_RPATH))
+            shutil.copy(MAIN_ABS_RPATH, copy)
+
+            rewriter = ELFRewriter(copy, force_dt_rpath=True)
+            rewriter.rpaths.insert(0, "floupiga")
+            rewriter.commit()
+
+            rewriter = ELFRewriter(copy)
+            self.assertEqual(rewriter.rpaths, ["floupiga"] + FILES_TO_RPATHS[MAIN_ABS_RPATH])
+            self.assertEqual(rewriter.rpaths_string, "floupiga:/yomama")
+
+            with open(copy, "rb") as fp:
+                elf_handle = ELFFile(fp)
+                self.assertEqual(_get_rpath_entry(elf_handle), "floupiga:/yomama")
